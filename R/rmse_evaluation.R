3cfa6aece1ae1f672b44dbf5fe7222096f466c10
Boris Demeshev <boris.demeshev@gmail.com>
2017-11-01 12:02:14 +0300

    attach y to forecasts

--- R/rmse_evaluation.R
+++ R/rmse_evaluation.R
@@ -29,3 +29,40 @@ get_forecasts_from_fit_files <- function(fit_file_names = NULL, basefolder) {
   }
   return(all_forecasts)
 } 
+
+#' Attach real y to all_forecasts data set. 
+#' 
+#' Add new column to all_forecasts with short model description and actual y.
+#' 
+#' Original all_forecasts contains row_number, model_id, variable and value columns. 
+#' Using info from fits_long this function attach actual y. 
+#' 
+#' @param all_forecasts tibble that contains row_number, model_id, variable and value columns.
+#' @param fits_long tibble that contains T_end, model_type and model_filename columns
+#' @param y actual time series
+#' @export 
+#' @return all_forecasts with new column
+attach_y_to_forecasts <- function(all_forecasts, fits_long, y) {
+  fits_long <- dplyr::mutate(fits_long, 
+                             model_id = as.numeric(stringr::str_extract(model_filename, "[0-9]+")))
+  # dirty hack to remove notes in R CMD check
+  model_filename <- NULL
+  T_end <- NULL
+  model_type <- NULL
+  model_id <- NULL
+  row_number <- NULL
+  
+  fits_long <- dplyr::select(fits_long, T_end, model_type, model_id)
+  all_forecasts <- dplyr::left_join(all_forecasts, fits_long, by = "model_id")
+  all_forecasts <- dplyr::mutate(all_forecasts, T_forecast = T_end + row_number)
+  y <- dplyr::as_tibble(y)
+  y <- dplyr::mutate(y, T_forecast = 1:nrow(y))
+  
+  y_melted <- reshape2::melt(y, id.vars = "T_forecast", value.name = "y_actual")
+  all_forecasts <- dplyr::left_join(all_forecasts, y_melted, by = c("T_forecast", "variable"))
+  return(all_forecasts)
+}
+
+
+
+
